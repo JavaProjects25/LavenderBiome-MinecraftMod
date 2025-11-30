b6e2fbe1ee33bafa3487adaeb7abfcb8514244f3
Marcel <38385813+MarcelTaveau@users.noreply.github.com>
2025-11-30 18:20:21 -0500

    Update ModPlacedFeatures.java
    Co-Authored-By: Santiago Santos <santysantos533@gmail.com>

--- src/main/java/sm/lavenderbiome/world/ModPlacedFeatures.java
+++ src/main/java/sm/lavenderbiome/world/ModPlacedFeatures.java
@@ -4,11 +4,12 @@ import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.ConstantIntProvider;
 import net.minecraft.world.gen.YOffset;
 import net.minecraft.world.gen.feature.*;
-import net.minecraft.world.gen.placementmodifier.HeightRangePlacementModifier;
-import net.minecraft.world.gen.placementmodifier.PlacementModifier;
+import net.minecraft.world.gen.placementmodifier.*;
 import sm.lavenderbiome.LavenderBiome;
 import sm.lavenderbiome.block.ModBlocks;
 
